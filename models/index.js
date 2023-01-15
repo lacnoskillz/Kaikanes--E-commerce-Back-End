1c8d4dcff7be88d0034731cbf9865bc5ce446427
lacnoskillz <kaikane3457@hotmail.com>
2023-01-15 01:33:16 -0600

    started index.js for the models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+})
 module.exports = {
   Product,
   Category,
